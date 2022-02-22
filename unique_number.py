d777bcfd49f6c1f4efb0e1eab4207a4d51fb99d4
finite <finite.impulse@gmail.com>
2022-02-22 15:11:36 +0100

    Move code example to code folder

--- unique_number.py
+++
@@ -1,9 +0,0 @@
-# Given a list of numbers where every numbers occurs twice, find that unique number
-
-
-# Bitwise XOR : Time Complexity : O(n)
-def singleNumber(nums):
-    res = 0
-    for num in nums:
-        res = res ^ num
-    return res
